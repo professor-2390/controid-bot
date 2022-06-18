e3455ea14643dd7ca8c49ceb272a8a40a8809bd6
Professor-2390 <sadmansakibkhan4@gmail.com>
2022-06-18 16:14:23 +0600

    Guild events and database added

---
+++ Commands/Moderation/unban.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Keyv = require('keyv');
+const bannedUsers = new Keyv(process.env.bannedUsers);
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('unban')
+    .setDescription(`Unbans a user earlier.`)
+    .addStringOption((option) => option
+      .setName('username')
+      .setDescription(`The username of the banned user.`)
+      .setRequired(true)
+    ),
+  requiredPerms: ['BAN_MEMBERS'],
+  botRequiredPerms: ['BAN_MEMBERS'],
+  async execute(interaction) {
+    const username = interaction.options.getString('username');
+    const bannedUsersArr = await bannedUsers.get(interaction.guild.id);
+    const bannedUser = bannedUsersArr.find((user) => user.username === username);
+    if (!bannedUser) {
+      return interaction.reply({ content: `${username} isn't banned.`, ephemeral: true });
+    }
+
+    let error = 0;
+    await interaction.guild.members.unban(bannedUser.userID).catch(async (err) => error = err);
+    if (error) {
+      return interaction.reply({ content: `${username} isn't banned.`, ephemeral: true });
+    }
+
+    bannedUsersArr.splice(bannedUsersArr.indexOf(bannedUser), 1);
+    await bannedUsers.set(interaction.guild.id, bannedUsersArr);
+    await interaction.reply(`${username} has been unbanned earlier.`);
+  }
+}
