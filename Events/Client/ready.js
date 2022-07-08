cffca8d96c22650062d3e8bbf30a45398b8cd062
Professor-2390 <sadmansakibkhan4@gmail.com>
2022-07-08 18:44:44 +0600

    Added database

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,10 +1,23 @@
 const chalk = require("chalk");
+const mongoose = require("mongoose");
 
 module.exports = {
     name: "ready",
     once:true,
     async execute(client) {
-        console.log(chalk.green(chalk.underline("🔴 Bot Online!")));
         await client.user.setPresence({ activities: [{ name: '/help', type:"LISTENING" }], status: 'dnd' });
+        
+        const Database = process.env.database;
+        
+        if (!Database) return;
+        mongoose.connect(Database, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(()=> {
+            console.log(chalk.green(chalk.underline("🟢 Successfully connected to database!")))
+        }).catch((err)=>{
+            console.log(chalk.red(err))
+        })
+        console.log(chalk.green(chalk.underline("🔴 Bot Online!")));
     }
 };
