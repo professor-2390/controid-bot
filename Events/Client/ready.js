e916495af703378ab6f7f35555dc646222756747
Professor-2390 <sadmansakibkhan4@gmail.com>
2022-07-19 22:58:10 +0600

    Client status command

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,23 +1,61 @@
+const os = require("os");
 const chalk = require("chalk");
+const ms = require("ms");
 const mongoose = require("mongoose");
 
+const DB = require("../../Schemas/ClientDB");
+
+async function getMemoryUsage() {
+  return process.memoryUsage().heapUsed / (1024 * 1024).toFixed(2);
+}
+
 module.exports = {
-    name: "ready",
-    once:true,
-    async execute(client) {
-        await client.user.setPresence({ activities: [{ name: '/help', type:"LISTENING" }], status: 'dnd' });
-        
-        const Database = process.env.database;
-        
-        if (!Database) return;
-        mongoose.connect(Database, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }).then(()=> {
-            console.log(chalk.green(chalk.underline("🟢 Successfully connected to database!")))
-        }).catch((err)=>{
-            console.log(chalk.red(err))
-        })
-        console.log(chalk.green(chalk.underline("🔴 Bot Online!")));
-    }
+  name: "ready",
+  once: true,
+  async execute(client) {
+    client.user.setPresence({
+      activities: [{ name: "/help", type: "LISTENING" }],
+      status: "dnd",
+    });
+
+    const Database = process.env.database;
+    if (!Database) return;
+    mongoose
+      .connect(Database, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log(
+          chalk.green(chalk.underline("🟢 Successfully connected to database!"))
+        );
+      })
+      .catch((err) => {
+        console.log(chalk.red(err));
+      });
+    console.log(chalk.green(chalk.underline("🔴 Bot Online!")));
+
+    let memArray = [];
+
+    setInterval(async () => {
+
+      memArray.push(await getMemoryUsage());
+
+      if (memArray.length >= 14) {
+        memArray.shift();
+      }
+
+      await DB.findOneAndUpdate(
+        {
+          Client: true,
+        },
+        {
+          Memory: memArray,
+        },
+        {
+          upsert: true,
+        }
+      );
+    }, ms("5s"));
+  },
 };
